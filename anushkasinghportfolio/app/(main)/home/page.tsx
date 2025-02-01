431fa72b6367618764f995411bfe164cb980edd0
Satvik Shubham Singh <58393226+satvikshubham@users.noreply.github.com>
2025-02-02 01:09:44 +0530

    main page

--- anushkasinghportfolio/app/(main)/home/page.tsx
+++ anushkasinghportfolio/app/(main)/home/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import Myphoto from "@/public/myphoto.jpg"
+import Image from "next/image";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function HomePage() {
+    const router = useRouter();
     return (
-        <>This is home me</>
+        <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
+            <Image src={Myphoto} alt="Anushka Singh" />
+            <Card>
+                <CardHeader>
+                    <CardTitle>Anushka Singh</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    Here is my description
+                </CardContent>
+                <CardFooter>
+                    <Button onClick={() => { router.push("/projects") }}>Projects</Button>
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
