80b0fe63ed85410677d5cea63074f9581761ea90
Anushka <anushka.singh1597@gmail.com>
2025-02-27 13:12:15 -0600

    file change

--- anushkasinghportfolio/app/(main)/projects/page.tsx
+++ anushkasinghportfolio/app/(main)/projects/page.tsx
@@ -5,8 +5,8 @@ export default function Projects() {
         <div className="items-center flex flex-col gap-4">
             <span className="text-2xl font-bold">Projects</span>
             <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
-                <ProjectTile image="/myphoto.jpg" projectHeadline="Jai Shree ram" projectLink="/projects/jaishreeram" />
-                <ProjectTile image="/myphoto.jpg" projectHeadline="Jai Shree Krishna" projectLink="/projects/jaishreekrishna" />
+                <ProjectTile image="/myphoto.jpg" projectHeadline="Shein" projectLink="/projects/shein" />
+                <ProjectTile image="/myphoto.jpg" projectHeadline="Splitwise" projectLink="/projects/splitwise" />
                 <ProjectTile image="/myphoto.jpg" projectHeadline="Jai Shree ram" projectLink="/projects/jaishreeram" />
                 <ProjectTile image="/myphoto.jpg" projectHeadline="Jai Shree Krishna" projectLink="/projects/jaishreekrishna" />
                 <ProjectTile image="/myphoto.jpg" projectHeadline="Jai Shree ram" projectLink="/projects/jaishreeram" />
